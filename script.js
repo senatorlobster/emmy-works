9a9901c4626d0c4dabd917cd326da9278563535e
senatorlobster <emmy@yardley.ca>
2025-08-13 12:52:41 -0400

    Fix brand hover effects: make both lightbulb and text clickable with proper animations

--- script.js
+++ script.js
@@ -57,13 +57,13 @@ function createFallbackNavigation() {
         <nav class="navbar">
             <div class="nav-container">
                 <div class="nav-brand">
-                    <div class="logo-container">
-                        <svg class="logo" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                        </svg>
-                        <div class="logo-glow"></div>
-                    </div>
                     <a href="index.html" class="brand-link">
+                        <div class="logo-container">
+                            <svg class="logo" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                            </svg>
+                            <div class="logo-glow"></div>
+                        </div>
                         <span class="brand-text">Emmy Yardley</span>
                     </a>
                 </div>
